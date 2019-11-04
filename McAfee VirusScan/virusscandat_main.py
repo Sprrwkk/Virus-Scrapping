92bdfac6c19c0099b9c68159f88b6f395e8cbcbc
Sprrwkk <sprrwkk@gmail.com>
2019-11-04 22:05:15 +0100

    VirusScanDat scrapping function

--- McAfee VirusScan/virusscandat_main.py
+++ McAfee VirusScan/virusscandat_main.py
@@ -1,25 +1,34 @@
 from urllib.request import urlopen
 import requests
-from zipfile import ZipFile
 import re
+import zipfile
+import json
 
-#str = 'avvdat-9425.zip'
 
+#main website url
 soup = requests.get('http://update.nai.com/Products/CommonUpdater/')
+#create soup_content object to get text format
 soup_content = soup.text
-print(soup_content)
+#create list with all matching queries (avvdat-~) using re library
+files = re.findall("avvdat-\w[0-9]\w[0-9].zip", soup_content)
 
 
-x = re.search(r"\Aavvdat-", soup_content)
-print(x.string)
+#get every file from files with re matching and download it
+for i in set(files):
+    zipsource = 'http://update.nai.com/Products/CommonUpdater/' + i
+    zipresp = urlopen(zipsource)
+    tempzip = open(i, "wb")
+    tempzip.write(zipresp.read())
+    tempzip.close()
 
 
-#zipsource = 'http://update.nai.com/Products/CommonUpdater/' + x.string
+#extract files and save all information in json file
+x = set(files)
+for file in x:
+    #print(file)
+    zip = zipfile.ZipFile(file)
+    x = zip.getinfo("avvscan.dat")
+    #print("------")
+    with open(f"{file[:11]}.json", 'w') as f_obj:
+        json.dump(str(x), f_obj)
 
-
-
-#zipresp = urlopen(zipsource)
-
-#tempzip = open(x.string, "wb")
-#tempzip.write(zipresp.read())
-#tempzip.close()
