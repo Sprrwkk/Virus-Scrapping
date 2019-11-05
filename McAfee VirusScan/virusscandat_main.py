d69d7b03d68e8441f7b61702d5b182752744cc3d
Sprrwkk <sprrwkk@gmail.com>
2019-11-05 19:29:50 +0100

    Change regex and refactoring script to functions

--- McAfee VirusScan/virusscandat_main.py
+++ McAfee VirusScan/virusscandat_main.py
@@ -10,25 +10,31 @@ soup = requests.get('http://update.nai.com/Products/CommonUpdater/')
 #create soup_content object to get text format
 soup_content = soup.text
 #create list with all matching queries (avvdat-~) using re library
-files = re.findall("avvdat-\w[0-9]\w[0-9].zip", soup_content)
+files = re.findall("avvdat-\d{4,6}\.zip", soup_content)
+
+def download_zip_file():
+    """Function that download a avvdat-zip file"""
+    #get every file from files with re matching and download it
+    for i in set(files):
+        zipsource = 'http://update.nai.com/Products/CommonUpdater/' + i
+        zipresp = urlopen(zipsource)
+        tempzip = open(i, "wb")
+        tempzip.write(zipresp.read())
+        tempzip.close()
 
 
-#get every file from files with re matching and download it
-for i in set(files):
-    zipsource = 'http://update.nai.com/Products/CommonUpdater/' + i
-    zipresp = urlopen(zipsource)
-    tempzip = open(i, "wb")
-    tempzip.write(zipresp.read())
-    tempzip.close()
-
-
+def extract_file():
+    """Function that extract avvdat-zip file and save information about this file in .json"""
 #extract files and save all information in json file
-x = set(files)
-for file in x:
-    #print(file)
-    zip = zipfile.ZipFile(file)
-    x = zip.getinfo("avvscan.dat")
-    #print("------")
-    with open(f"{file[:11]}.json", 'w') as f_obj:
-        json.dump(str(x), f_obj)
+    x = set(files)
+    for file in x:
+        #print(file)
+        zip = zipfile.ZipFile(file)
+        x = zip.getinfo("avvscan.dat")
+        #print("------")
+        with open(f"{file[:11]}.json", 'w') as f_obj:
+            json.dump(str(x), f_obj)
 
+
+download_zip_file()
+extract_file()
